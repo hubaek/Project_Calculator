b961a73f6cfbb1a110500c1cd1499e4c282c5693
hubaek <zjfl3122@naver.com>
2024-09-06 20:28:18 +0900

    Feat : Scanner로 사칙연산 계산기 입력 받기(if문) - Scanner로 정수두개(fristInputNum, lastInputNum), 사칙연산 기호(oper) 입력받기 - if문으로 oper의 각 기호마다 사칙연산 계산 - 가독성 위해서 switch문 고려예정 - while문으로 계산기 반복 예정

--- src/com/project/calculator/Calculator.java
+++ src/com/project/calculator/Calculator.java
@@ -4,6 +4,34 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+
+        // Scanner활용 - 양의 정수(0포함) 입력받기
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫번째 숫자를 입력해주세요 : ");
+        int firstInputNum = sc.nextInt();
+
+        sc.nextLine(); // nextInt에서 입력받은 줄바꿈을 제거, 버퍼 비우기
+
+        // 사칙연산 기호( + , - , * , / ) 입력받기
+        // 4가지 기호 외에 들어오면 정확한 기호를 입력해 주세요 경고하고, 다시 입력 필요
+        System.out.println("계산할 기호를 입력해주세요(+,-,*,/) : ");
+        char oper = sc.nextLine().charAt(0);
+
+        System.out.println("두번째 숫자를 입력해주세요 : ");
+        int lastInputNum = sc.nextInt();
+
+//        System.out.println(firstInputNum + " + " + lastInputNum + " = " + (firstInputNum + lastInputNum));
+
+        if (oper == '+') {
+            System.out.println(firstInputNum + " + " + lastInputNum + " = " + (firstInputNum + lastInputNum));
+        } else if (oper == '-') {
+            System.out.println(firstInputNum + " - " + lastInputNum + " = " + (firstInputNum - lastInputNum));
+        } else if (oper == '*') {
+            System.out.println(firstInputNum + " * " + lastInputNum + " = " + (firstInputNum * lastInputNum));
+        } else if (oper == '/') {
+            System.out.println(firstInputNum + " / " + lastInputNum + " = " + (firstInputNum / lastInputNum));
+        }
+
+
     }
 }
