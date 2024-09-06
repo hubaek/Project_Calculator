d86ccd628c9c6a45fb494cb2c9fecb276c12b851
hubaek <zjfl3122@naver.com>
2024-09-06 17:02:15 +0900

    init project

---
+++ src/com/project/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.project.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello, Calculator!");
+    }
+}
