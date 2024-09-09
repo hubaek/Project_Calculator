b75865120a24da3dc06993ef6cc55b83cb3d5186
hubaek <zjfl3122@naver.com>
2024-09-09 22:45:23 +0900

    Refactor : 클래스 생성(객체지향), 사칙연산 메서드 생성 - Calculator 클래스 생성 - 사칙연산 메서드 생성

--- src/com/project/calculator/Calculator.java
+++ src/com/project/calculator/Calculator.java
@@ -3,7 +3,54 @@ package com.project.calculator;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    Scanner sc = new Scanner(System.in);
+
+    // 필드 선언부
+    public int firstInputNum; // 첫번째 입력 정수
+    public int lastInputNum; // 두번째 입력 정수
+    public char oper;   // 연산 기호
+
+    // 메서드 선언부
+    // 더하기 메서드
+    int add(int firstInputNum, int lastInputNum, char oper) {
+        if (oper == '+') {
+            return firstInputNum + lastInputNum;
+        }
+        return 0;
     }
+
+    // 빼기 메서드
+    int subtract(int firstInputNum, int lastInputNum, char oper) {
+        if (oper == '-') {
+            return firstInputNum - lastInputNum;
+        }
+        return 0;
+    }
+
+    // 곱하기 메서드
+    int multiply(int firstInputNum, int lastInputNum, char oper) {
+        if (oper == '*') {
+            while (lastInputNum == 0) {
+                System.out.println("0으로 곱할 수 없습니다. 다시 입력해주세요 : ");
+                lastInputNum = sc.nextInt();
+            }
+            return firstInputNum * lastInputNum;
+        }
+        return 0;
+    }
+
+    // 나누기 메서드
+    int divide(int firstInputNum, int lastInputNum, char oper) {
+        if (oper == '/') {
+            while (lastInputNum == 0) {
+                System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요 : ");
+                lastInputNum = sc.nextInt();
+            }
+            return firstInputNum / lastInputNum;
+        }
+        return 0;
+    }
+
+
+
 }
