9b50ab30ae3f77c43dde5c79cca41e6707fa3553
hubaek <zjfl3122@naver.com>
2024-09-10 11:59:03 +0900

    Refactor : 사칙연산 method 리팩토링, 연산결과 저장 컬렉션 생성 - 사칙연산 4개 메서드 -> arithmeticOperation 메서드로 통합 - IllegalArgumentException예외 적용 : 잘못된 인수가 메서드에 전달될때 발생하는 런타임 예외 -> *, / 를 0으로 했을 경우, 연산기호를 잘못 적었을 경우 발생 - 연산결과 arrayList(resultList)에 저장 - 연산 결과 반환 메서드 생성(getResultList)

--- src/com/project/calculator/Calculator.java
+++ src/com/project/calculator/Calculator.java
@@ -1,55 +1,58 @@
 package com.project.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
     Scanner sc = new Scanner(System.in);
 
-    // 필드 선언부
-    public int firstInputNum; // 첫번째 입력 정수
-    public int lastInputNum; // 두번째 입력 정수
-    public char oper;   // 연산 기호
+    // 필드 선언부 -> 메서드에서 매개변수로 받기에 필요 없다?
+//    public int firstInputNum; // 첫번째 입력 정수
+//    public int lastInputNum; // 두번째 입력 정수
+//    public char operator;   // 연산 기호
+
+    // 연산 결과를 저장하는 컬렉션 타입 필드
+    private List<Double> resultList = new ArrayList<Double>();
+
+
 
     // 메서드 선언부
-    // 더하기 메서드
-    int add(int firstInputNum, int lastInputNum, char oper) {
-        if (oper == '+') {
-            return firstInputNum + lastInputNum;
-        }
-        return 0;
-    }
+    public double arithmeticOperation(int firstInputNum, int lastInputNum, char operator) {
+        double result;
 
-    // 빼기 메서드
-    int subtract(int firstInputNum, int lastInputNum, char oper) {
-        if (oper == '-') {
-            return firstInputNum - lastInputNum;
-        }
-        return 0;
-    }
-
-    // 곱하기 메서드
-    int multiply(int firstInputNum, int lastInputNum, char oper) {
-        if (oper == '*') {
-            while (lastInputNum == 0) {
-                System.out.println("0으로 곱할 수 없습니다. 다시 입력해주세요 : ");
-                lastInputNum = sc.nextInt();
+        switch (operator) {
+            case '+' -> result = firstInputNum + lastInputNum;
+            case '-' -> result = firstInputNum - lastInputNum;
+            case '*' -> {
+                if (lastInputNum == 0) {
+                    throw new IllegalArgumentException("0으로 곱할 수 없습니다.");
+                } else {
+                    result = firstInputNum * lastInputNum;
+                }
             }
-            return firstInputNum * lastInputNum;
+            case '/' -> {
+                if (lastInputNum == 0) {
+                    throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+                } else {
+                    result = (double) firstInputNum / lastInputNum;
+                }
+            }
+
+            default -> throw new IllegalArgumentException("잘못된 연산자입니다.");
         }
-        return 0;
+
+        // 연산 결과를 컬렉션에 저장
+        resultList.add(result);
+        return result;
+
     }
 
-    // 나누기 메서드
-    int divide(int firstInputNum, int lastInputNum, char oper) {
-        if (oper == '/') {
-            while (lastInputNum == 0) {
-                System.out.println("0으로 나눌 수 없습니다. 다시 입력해주세요 : ");
-                lastInputNum = sc.nextInt();
-            }
-            return firstInputNum / lastInputNum;
-        }
-        return 0;
+    // 연산 결과를 반환하는 메서드
+    public List<Double> getResultList(){
+        return resultList;
     }
+
 
 
 
